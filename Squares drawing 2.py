0164e3f1eb031e4cb296555a202ef04aeb8cf479
Markadsk <55198013+Markadsk@users.noreply.github.com>
2019-09-17 11:52:25 -0400

    Add files via upload
    my final drawing, not yet perfect but still very good!!!

--- Squares drawing 2.py
+++ Squares drawing 2.py
@@ -3,26 +3,34 @@ from turtle import *
 fillcolor('purple')
 pensize(10)
 pencolor('black')
-speed(8)
+speed(0)
 
+for cubeplusplusplus in range(2):
+    for cubeplusplus in range(2):
+        for cubeplus in range(2):
+            for cube in range(3):
+                for square in range(3):
+                    begin_fill()
+                    forward(100)
+                    left(90)
+                    forward(100)
+                    left(90)
+                    forward(100)
+                    left(90)
+                    forward(100)
+                    left(90)
+                    end_fill()
+                    forward(100)
 
-for cube in range(3):
-    for square in range(3):
-        begin_fill()
-        forward(100)
-        left(90)
-        forward(100)
-        left(90)
-        forward(100)
-        left(90)
-        forward(100)
-        left(90)
-        end_fill()
-        forward(100)
+                left(180)
+                forward(300)
+                right(90)
+                forward(100)
+                right(90)
 
-    left(180)
-    forward(300)
+            home()
+            right(90)
+
+        right(90)
+
     right(90)
-    forward(100)
-    right(90)
-
